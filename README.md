56dad30797e1cb85fc55d0f197add37a92321bb3
Reisbel Machado <reisbel@gmail.com>
2019-08-20 15:07:28 -0400

    Add issue example source code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # vcloud-auth-issue
+
+## Steps
+
+Clone repository
+
+```bash
+git clone https://github.com/reisbel/vcloud-auth-issue.git
+```
+
+Update app.config credentials
+
+```xml
+ <add key="vcloud.user" value="user@org"/>
+ <add key="vcloud.pass" value="REPLACE WITH PASSWORD"/>
+ <add key="vcloud.url" value="https://cloud.ussignalcom.net"/>
+ ```
+
+Open solution using Visual Studio and execute the application
+
+Output error
+
+```output
+Error: The given key was not present in the dictionary.
+```
+
+## Dependencies
+
+Visual Studio
+<https://visualstudio.microsoft.com/vs/>
+
+vCloud SDK for .Net, version 5.5.0-1294396
+<https://code.vmware.com/web/sdk/5.5.0-1294396/vcloud-dotnet>
+
+## License
+
+MIT - See [LICENSE](LICENSE) for more information.
---
+++ vCloudIssue/Program.cs
@@ -0,0 +1,29 @@
+﻿using com.vmware.vcloud.sdk;
+using System;
+using System.Configuration;
+
+namespace Itopia.vCloudIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = ConfigurationManager.AppSettings["vcloud.user"];
+            string password = ConfigurationManager.AppSettings["vcloud.pass"];
+            string url = ConfigurationManager.AppSettings["vcloud.url"];
+
+            var vcloudClient = new vCloudClient(url, com.vmware.vcloud.sdk.constants.Version.V5_5);
+
+            try
+            {
+                vcloudClient.Login(username, password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
